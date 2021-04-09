8e7ef9a5d3a0fe73f0aa08d7a244091cf1c73f2a
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-04-09 12:03:36 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/yukon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yukon.dir/main.c.o"
+  "yukon"
+  "yukon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/yukon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/yukon.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for yukon.
+# This may be replaced when dependencies are built.
