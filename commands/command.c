4f50517038160ac766bcdc2a8f54f8a5ed3197bd
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-04-16 13:31:14 +0200

    added command header and source with empty functions GetCommandInput and ReadString

---
+++ commands/command.c
@@ -0,0 +1,19 @@
+//
+// Created by Tobias Maneschijn, on 16/04/2021.
+//
+
+
+
+
+#include "command.h"
+
+void GetCommandInput() {
+
+    return NULL;
+}
+
+char *ReadString() {
+    return NULL;
+}
+
+
---
+++ commands/command.h
@@ -0,0 +1,22 @@
+//
+// Created by Tobias Maneschijn on 16/04/2021.
+//
+
+#ifndef YUKON_COMMAND_H
+#define YUKON_COMMAND_H
+
+
+/**
+ * Returns a command corresponding to the given string.
+ * @author Tobias Maneschijn, s205422
+ */
+void GetCommandFromString(char* str);
+
+/**
+ * Reads a string
+ * @author Tobias Maneschijn, s205422
+ */
+
+char* ReadString();
+
+#endif //YUKON_COMMAND_H
