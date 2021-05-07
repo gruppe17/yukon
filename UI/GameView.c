cd656f68b222d1992472b4fedb74be9910e7a118
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-05-07 15:50:12 +0200

    Added GameView.c and GameView.h

---
+++ UI/GameView.c
@@ -0,0 +1,10 @@
+//
+// Created by Nylander on 07-05-2021.
+//
+
+#include "GameView.h"
+
+
+char* gameToDisplayString(Game game){
+
+}
---
+++ UI/GameView.h
@@ -0,0 +1,12 @@
+//
+// Created by Nylander on 07-05-2021.
+//
+
+#include "../game/game.h"
+
+#ifndef YUKON_GAMEVIEW_H
+#define YUKON_GAMEVIEW_H
+
+char* gameToDisplayString(Game game);
+
+#endif //YUKON_GAMEVIEW_H
