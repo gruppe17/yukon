ae14eb1250137f8eb6f45829b59ab0a504d9b7d4
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-05-08 10:11:50 +0200

    Added GameViewConstants.h

---
+++ UI/GameViewConstants.h
@@ -0,0 +1,36 @@
+//
+// Created by Nylan on 08-05-2021.
+//
+
+#ifndef YUKON_GAMEVIEWCONSTANTS_H
+#define YUKON_GAMEVIEWCONSTANTS_H
+
+/**
+ * The prefix of the header for every column
+ * @author Rasmus Nylander, s205418
+ */
+const char columnPrefix[] = "C";
+/**
+ * Spacer used to separate different columns
+ * @author Rasmus Nylander, s205418
+ */
+const char columnSpacer[] = "\t";
+/**
+ * Spacer between the column headers and the rest of the view.
+ * Written at the end of the column headers
+ * @author Rasmus Nylander, s205418
+ */
+const char headerSuffix[] = "\n\n";
+/**
+ * String used instead of card when the card should be
+ * hidden/face down.
+ * @author Rasmus Nylander, s205418
+ */
+const char hiddenCardText[] = "[]";
+/**
+ * Spacer used to separate different columns
+ * @author Rasmus Nylander, s205418
+ */
+const char rowSuffix[] = "\n";
+
+#endif //YUKON_GAMEVIEWCONSTANTS_H
