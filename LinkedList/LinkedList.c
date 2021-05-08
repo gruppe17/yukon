68812358cbe406702804d014669bd7da95219413
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-05-08 16:44:41 +0200

    Added LinkedListInternalFunctions.h

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -3,32 +3,14 @@
 //
 
 #include "LinkedList.h"
+#include "LinkedListInternalFunctions.h"
 #include <stdlib.h>
 #include <time.h>
 
-
-typedef struct _node{
+struct _node{
 	struct _node* next;
 	void* data;
-} Node;
-
-/**
- * Creates a new node containing the specified data and returns
- * a pointer to it or NULL if memory allocation failed.
- * @param data a pointer to the data the nodes should contain
- * @return a pointer to a new node containing the specified data
- * 			or NULL if memory allocation fails
- * @author Rasmus Nylander, s205418
- */
-Node* newNode(void* data);
-/**
- * Extract the data of a specified node and returns it.
- * If the node is NULL function returns NULL,
- * @param node the node to extract data from
- * @return the data contained in the node or NULL if the node is NULL
- * @author Rasmus Nylander, s205418
- */
-void* extractData(Node* node);
+};
 
 Node* newNode(void* data){
 	Node *node = (Node *) malloc(sizeof(Node));
@@ -36,16 +18,6 @@ Node* newNode(void* data){
 	node->data = data;
 	return node;
 }
-
-
-
-Node* getLastNode(LinkedList *linkedList);
-Node* getNodePtr(LinkedList *linkedList, int index);
-Node** getNodePtrPtr(LinkedList *linkedList, int index);
-
-bool appendToEmpty(LinkedList *appendTo, LinkedList *appending);
-
-bool swapNodes(Node **a, Node **b);
 
 struct linkedList {
 	int size;
